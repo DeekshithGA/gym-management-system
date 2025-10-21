69b9584cd76467fddbd8de75bb2263fd719baa0a
DeekshithGA <venmathi.g29@gmail.com>
2025-10-21 07:50:11 +0530

    Delete js directory

--- js/logging.js
+++
@@ -1,5 +0,0 @@
-export function logEvent(event, data) {
-  console.log(`[LOG][${new Date().toISOString()}] ${event}`, data);
-  // Optionally send to Firestore for centralized logging
-  // For real app, analyze logs to alert admins on suspicious patterns
-}
